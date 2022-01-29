0381fb3840b9366c89b635ca8763a9fb4f80bc75
priteshvermaa <pritesh0797@gmail.com>
2022-01-29 12:47:50 +0530

    Used map for better storage utilization and modified algorithm to work for disjoint graph :art:

--- DFS_trav.cpp
+++ DFS_trav.cpp
@@ -2,50 +2,41 @@
 
 class Graph{
     int n; //no fo vertices
-    std::list<int>*adj; //list to strore adjacent vertices
-    public:
-    Graph(int v);
+    std::map<int, bool> visited;
+    std::map<int, std::list<int>> adj; //list to strore adjacent vertices
+    public:    
     void addEdge(int u, int v);
-    void DFS(int s);
-    void DFSUtil(int v, bool* visited);
+    void DFS();
+    void DFSUtil(int v);
 };
 
-Graph::Graph(int v){
-    this->n = v;
-    this->adj = new std::list<int>[v];
-}
+
 
 void Graph::addEdge(int u, int v){
     // This function adds the edge from vertex u to v
     adj[u].push_back(v);
 }
 
-void Graph::DFS(int s){
+void Graph::DFS(){
     // This is called first time for the starting vertex
-    bool visited[n];
-    for(int i = 0; i < n; ++i)
-        visited[i] = false;
-    std::cout<<"DFS traversal: \n"<< s <<" ";
-    visited[s] = true;
-    std::list<int>::iterator i;
-    for(i = adj[s].begin(); i!=adj[s].end(); i++)
-        if(visited[*i] == false)
-            DFSUtil(*i, visited);  
+    for(auto i : adj)
+        if(visited[i.first] == false)
+            DFSUtil(i.first);
 
 }
 
-void Graph::DFSUtil(int v, bool * visited){
+void Graph::DFSUtil(int v){
     //This function is called after starting vertex
     visited[v] = true;
     std::cout << v << " ";
     std::list<int>::iterator i;
      for(i = adj[v].begin(); i!=adj[v].end(); i++)
         if(visited[*i] == false)
-            DFSUtil(*i, visited);
+            DFSUtil(*i);
 }
 
 int main(){
-    Graph g(10);
+    Graph g;
     g.addEdge(0, 1);
     g.addEdge(0, 9);
     g.addEdge(1, 2);
@@ -53,5 +44,5 @@ int main(){
     g.addEdge(2, 3);
     g.addEdge(9, 3);
 
-    g.DFS(0);// dfs on vertices 0;
+    g.DFS();// dfs on vertices 0;
 }
